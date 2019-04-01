203f863dac26900d362cab708bfaed71798a6f6a
swit <swithaoy@gmail.com>
2019-04-01 17:13:21 +0800

    map，string

--- map/map_test.go
+++ map/map_test.go
@@ -59,3 +59,45 @@ func TestRangeMap(t *testing.T) {
 		fmt.Println(key, value)
 	}
 }
+
+// Map 的 value 可以是一个方法
+func TestMapWithFuncValue(t *testing.T) {
+	m := map[int]func(num int) int{}
+
+	m[1] = func(num int) int {
+		return num
+	}
+	m[2] = func(num int) int {
+		return num * num
+	}
+	m[3] = func(num int) int {
+		return num * 2
+	}
+
+	fmt.Println(m[1](10))
+	fmt.Println(m[2](10))
+	fmt.Println(m[3](10))
+}
+
+// 利用 Map 实现 Set （Go 语言中没有 Set 的实现）
+func TestSet(t *testing.T) {
+	set := map[int]bool{}
+
+	//添加元素
+	set[10] = true
+
+	n := 3
+	//判断元素是否存在
+	if set[n] {
+		fmt.Printf("%d 存在\n", n)
+	} else {
+		fmt.Printf("%d 不存在\n", n)
+	}
+
+	set[3] = true
+	//判断元素个数
+	fmt.Println(len(set))
+
+	//删除元素
+	delete(set, 10)
+}
